771c7df41a4f9bc359e46388c0034b226917d758
Kala Foods <guhan.delta@gmail.com>
2021-05-31 00:01:39 -0500

    Minor Changes

--- src/pages/stree.js
+++ src/pages/stree.js
@@ -1,21 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import TreeView from "@material-ui/lab/TreeView";
 import TreeItem from "@material-ui/lab/TreeItem";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import Typography from "@material-ui/core/Typography";
-import MailIcon from "@material-ui/icons/Mail";
-import DeleteIcon from "@material-ui/icons/Delete";
-import Label from "@material-ui/icons/Label";
-import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
-import InfoIcon from "@material-ui/icons/Info";
-import ForumIcon from "@material-ui/icons/Forum";
-import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import ArrowRightIcon from "@material-ui/icons/ArrowRight";
-import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+// import Typography from "@material-ui/core/Typography";
+// import MailIcon from "@material-ui/icons/Mail";
+// import DeleteIcon from "@material-ui/icons/Delete";
+// import Label from "@material-ui/icons/Label";
+// import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+// import InfoIcon from "@material-ui/icons/Info";
+// import ForumIcon from "@material-ui/icons/Forum";
+// import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+// import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+// import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+// import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
 
 const data = {
   id: "root",
